3902cbac64e19f65b9e34ecffa2ce79bacec729e
Efsun Yusubzade <166283682+TelimEfsun@users.noreply.github.com>
2025-03-01 11:46:33 +0400

    Add files via upload

---
+++ Ders-2 Task 2/Ders-2 Task 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders_2_Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 5, 2, 1, 5, 2, 5, 3, 5, 6, 2, 1, 6, 2, 6, 2, 5, 3, 2, 7, 4, 6, 4, 5, 6, 2, 5, 6, 3, 7, 3, 7 };
+            int count = 0;
+
+            foreach (var num in arr)
+            {
+                if (num == 5)
+                {
+                    count++;
+                }
+
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
