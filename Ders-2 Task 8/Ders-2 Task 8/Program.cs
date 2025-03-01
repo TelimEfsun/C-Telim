f5b456ba6648c531bff4b1d7cfe8e89a57a109a8
Efsun Yusubzade <166283682+TelimEfsun@users.noreply.github.com>
2025-03-01 18:38:08 +0400

    Add files via upload

---
+++ Ders-2 Task 8/Ders-2 Task 8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders_2_Task_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 5, 2, 1, 5, 2, 5, 3, 5, 6, 2, 1, 6, 2, 6, 2, 5, 3, 2, 7, 4, 6, 4, 5, 6, 2, 5, 6, 3, 7, 3, 7 };
+
+            int MinIndex = Array.IndexOf(arr, 5);
+            int MaxIndex = Array.LastIndexOf(arr, 5);
+
+            Console.WriteLine($"En kicik index {MinIndex}");
+            Console.WriteLine($"En boyuk index {MaxIndex}");
+        }
+    }
+}
